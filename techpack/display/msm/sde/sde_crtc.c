98545020aa7fa7283d62a007bd69007211b6c1de
N0m4n904 <noah.anleitner@halogenos.org>
2024-07-28 11:46:25 +0200

    Revert "techpack: display: Adapt custom rom's fingerprint impl for NOS 2.5.5 changes"
    This reverts commit 2e44a348639525e33f0d14ef2f9e24906f2291d7.

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -5005,9 +5005,6 @@ sde_crtc_setup_fod_dim_layer(struct sde_crtc_state *cstate, uint32_t stage)
 
         alpha = dsi_panel_get_fod_dim_alpha(display->panel);
 
-        if (!alpha)
-                goto error;
-
 	dim_layer = &cstate->dim_layer[cstate->num_dim_layers];
 	dim_layer->flags = SDE_DRM_DIM_LAYER_INCLUSIVE;
 	dim_layer->stage = layer_stage;
@@ -5045,7 +5042,6 @@ sde_crtc_fod_atomic_check(struct sde_crtc_state *cstate,
 
 	for (plane_idx = 0; plane_idx < cnt; plane_idx++) {
 		if (sde_plane_is_fod_layer(pstates[plane_idx].drm_pstate)) {
-		        cstate->fod_pressed = true;
 #ifdef CONFIG_CPU_INPUT_BOOST
 			cpu_input_boost_kick_max(500);
 #endif
@@ -5053,13 +5049,10 @@ sde_crtc_fod_atomic_check(struct sde_crtc_state *cstate,
 			break;
 	        }
 	}
-
-	if (fod_plane_idx >= 0)
+	
+	if (fod_plane_idx >= 0) {
                 dim_layer_stage = pstates[fod_plane_idx].stage;
-        else
-                cstate->fod_pressed = false;
-
-        if (force_fod_ui && display->panel->power_mode == SDE_MODE_DPMS_ON) {
+        } else if (force_fod_ui && display->panel->power_mode == SDE_MODE_DPMS_ON) {
                 if (dim_layer_stage == INT_MAX) {
                         dim_layer_stage = 0;
                         for (plane_idx = 0; plane_idx < cnt; plane_idx++) {
@@ -5068,7 +5061,7 @@ sde_crtc_fod_atomic_check(struct sde_crtc_state *cstate,
                         }
                 }
 	}
-
+	
 	if (fod_plane_idx >= 0 || force_fod_ui)
 		fod_dim_layer = sde_crtc_setup_fod_dim_layer(cstate, dim_layer_stage);
 
