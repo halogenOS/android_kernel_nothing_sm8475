5be5a68beda73e92da840ac0be21cb429878e48b
Noah Anleitner <noah.anleitner@halogenos.org>
2025-02-20 21:25:47 +0100

    Add Kernel/common and Meteoric to add_upstream script
    Signed-off-by: Noah Anleitner <noah.anleitner@nonetwor.cc>

---
+++ add_upstream_remotes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Kernel
+git remote add msm-5.10 https://git.codelinaro.org/clo/la/kernel/msm-5.10
+git remote add common https://android.googlesource.com/kernel/common/
+git remote add meteoric https://github.com/HELLBOY017/kernel_nothing_sm8475/
